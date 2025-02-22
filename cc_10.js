5d5521503d7b19640595e50b9292077f6567897c
Aisha Nomani <aishan@usf.edu>
2025-02-21 19:45:53 -0500

    Task 1 - Created Product Class

---
+++ cc_10.js
@@ -0,0 +1,23 @@
+// Task 1: Creating a Product Class
+console.log("********** Created Product Class **********");
+class Product { 
+    constructor(name, id, price, stock) { 
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.stock = stock;
+    };
+    getDetails(){ 
+        return `Product: ${this.name}, ID: ${this.id}, Price: ${this.price}, Stock: ${this.stock}`; 
+    };
+    updateStock(quantity){
+        this.stock -= quantity;
+    };
+};
+const prod1 = new Product("Laptop", 101, 1200, 10); 
+console.log(prod1.getDetails()); //Expected Output
+prod1.updateStock(3);
+console.log(prod1.getDetails()); //Expected Output
+
+
+
